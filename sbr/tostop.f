fe255be3da4ef8e8b922331b223db6c92c66c256
zemtsov <ivan@zemtsov.pw>
2021-02-25 17:33:42 +0300

    Example files

---
+++ sbr/tostop.f
@@ -0,0 +1,53 @@
+      subroutine tostop
+
+      implicit none
+      include 'for/parameter.inc'
+      include 'for/const.inc'
+      include 'for/outcmn.inc'
+      include 'for/status.inc'
+      include	'for/timeoutput.inc'
+      integer i,j,ier
+      character filename*40
+ 
+         filename='dat/dynam.dat'
+                  ! print*,filename
+         CALL OPENWT(11,filename,0,ier)
+         write(11,'(17(A4,8X))')'Time',(NAMET(J),J=1,16)
+         do I=1,LTOUT-1
+            write(11,'(17(E11.5,1X))')TTOUT(I),(tout(I,J),J=1,16)
+         enddo
+         close(11)
+
+         filename='dat/radial.dat'
+                  ! print*,filename
+         CALL OPENWT(13,filename,0,ier)
+         write(13,'(8(A3,9X))')
+     >    'Rad',
+     >    'Prc',
+     >    'Prx',
+     >    'Van',
+     >    'Dan',
+     >    'Vnc',
+     >    'Dnc',
+     >    'n_W'
+         do J=1,NA1
+            write(13,'(8(E11.5,1X))')
+     >      AMETR(J),
+     >      CAR5(J),
+     >      PRADX(J),
+     >      CN(J),
+     >      DN(J),
+     >      WORK(J,997)*1.e-4,
+     >      WORK(J,998)*1.e-2,
+     >      WORK(J,1052)
+         enddo
+         close(13)
+      
+         print*,'Data saved in dynam.dat and radial.dat stored in /dat'
+
+
+C          CALL OPENWT(12,'dat/flagfile',0,ier)
+C          close(12)
+
+      stop
+      end
